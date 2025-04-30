ce0e422d2036d9524b646c3f656a4744c98fdb2b
Creepybits <zanno.jacklin@hotmail.com>
2025-04-30 12:17:44 +0200

    Add files via upload
    Minor fixes

--- __init__.py
+++ __init__.py
@@ -118,7 +118,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     **AudioKeywordExtractor_NODE_DISPLAY_NAME_MAPPINGS,
 }
 
-__version__ = "2.2.4"
+__version__ = "2.2.5"
 
 # Define the web directory for ComfyUI to find our JavaScript files
 WEB_DIRECTORY = "./web"
