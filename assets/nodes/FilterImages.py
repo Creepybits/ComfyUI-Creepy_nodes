6cd6d0a0d09481e4173764955c72274932da4078
Creepybits <zanno.jacklin@hotmail.com>
2025-06-30 17:06:27 +0200

    Update FilterImages.py

--- assets/nodes/FilterImages.py
+++ assets/nodes/FilterImages.py
@@ -6,7 +6,7 @@ class FilterImages:
     def INPUT_TYPES(s):
         return {"required": {"folder_path": ("STRING", {"default": ""}), }}
 
-    RETURN_TYPES = ("STRING",)  # Returns a list of image file paths
+    RETURN_TYPES = ("STRING",)  
     RETURN_NAMES = ("image_paths",)
     OUTPUT_NODE = True
     CATEGORY = "Creepybits/utilities"
@@ -14,12 +14,12 @@ class FilterImages:
     def filter_images(self, folder_path):
         if not os.path.isdir(folder_path):
             print(f"Error: '{folder_path}' is not a valid directory.")
-            return ([],)  # Return an empty list if the folder is invalid
+            return ([],)  
 
         image_paths = []
         for filename in os.listdir(folder_path):
             filepath = os.path.join(folder_path, filename)
-            if os.path.isfile(filepath) and imghdr.what(filepath) is not None:  # imghdr checks if the file is an image
+            if os.path.isfile(filepath) and imghdr.what(filepath) is not None: 
                 image_paths.append(filepath)
 
         return (image_paths,)
