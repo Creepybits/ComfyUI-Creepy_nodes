ba0e9dd9bccf8ac48d0553954876593151856ba4
Creepybits <zanno.jacklin@hotmail.com>
2025-06-30 16:46:09 +0200

    Update DynamicImageSwitch.py
    Cleaning up code

--- assets/nodes/DynamicImageSwitch.py
+++ assets/nodes/DynamicImageSwitch.py
@@ -21,24 +21,24 @@ class DynamicImageSwitch:
 
     def dynamic_switch(self, **kwargs):
         show_help = "Proverb of the day: Freedom means the right to yell, “THEATRE!” in a crowded fire."
-        latent = None  # Initialize model to None
+        latent = None  
 
-        if "image1" in kwargs and kwargs["image1"] is not None: #Check the kwarg model1 exists
-            image = kwargs["image1"]  # Use model1 if it exists
+        if "image1" in kwargs and kwargs["image1"] is not None: 
+            image = kwargs["image1"]  
 
-        elif "image2" in kwargs and kwargs["image2"] is not None: #Check the kwarg model2 exists, to use the model
-            image = kwargs["image2"]  # Use model2 if model1 is missing
+        elif "image2" in kwargs and kwargs["image2"] is not None: 
+            image = kwargs["image2"]  
 
-        elif "image3" in kwargs and kwargs["image3"] is not None: #Check the kwarg model2 exists, to use the model
-            image = kwargs["image3"]  # Use model2 if model1 is missing
+        elif "image3" in kwargs and kwargs["image3"] is not None: 
+            image = kwargs["image3"]  
 
-        if image is not None:  # Return the model if a valid one was found
+        if image is not None:  
             return (image, show_help,)
         else:
-            return (None, show_help,)  # Return None if no valid models were provided
+            return (None, show_help,) 
 
 
-NODE_CLASS_MAPPINGS = {  # <---Outdent these lines
+NODE_CLASS_MAPPINGS = { 
       "DynamicImageSwitch": DynamicImageSwitch,
 }
 
