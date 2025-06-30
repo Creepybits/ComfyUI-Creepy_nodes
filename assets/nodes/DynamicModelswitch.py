8a00077bc9f17e19baec3a1e57f00407f4a723e1
Creepybits <zanno.jacklin@hotmail.com>
2025-06-30 17:01:58 +0200

    Update DynamicModelswitch.py
    Cleaning up code

--- assets/nodes/DynamicModelswitch.py
+++ assets/nodes/DynamicModelswitch.py
@@ -22,27 +22,27 @@ class DynamicModelswitch:
 
     def dynamic_switch(self, **kwargs):
         show_help = "Proverb of the day: Freedom means the right to yell, “THEATRE!” in a crowded fire."
-        model = None  # Initialize model to None
+        model = None  
 
-        if "model1" in kwargs and kwargs["model1"] is not None: #Check the kwarg model1 exists
-            model = kwargs["model1"]  # Use model1 if it exists
+        if "model1" in kwargs and kwargs["model1"] is not None: 
+            model = kwargs["model1"] 
 
-        elif "model2" in kwargs and kwargs["model2"] is not None: #Check the kwarg model2 exists, to use the model
-            model = kwargs["model2"]  # Use model2 if model1 is missing
+        elif "model2" in kwargs and kwargs["model2"] is not None: 
+            model = kwargs["model2"] 
 
-        elif "model3" in kwargs and kwargs["model3"] is not None: #Check the kwarg model3 exists, to use the model
-            model = kwargs["model3"]  # Use model3 if model2 is missing
+        elif "model3" in kwargs and kwargs["model3"] is not None: 
+            model = kwargs["model3"]  
 
-        elif "model4" in kwargs and kwargs["model4"] is not None: #Check the kwarg model4 exists, to use the model
-            model = kwargs["model4"]  # Use model4 if model3 is missing
+        elif "model4" in kwargs and kwargs["model4"] is not None: 
+            model = kwargs["model4"]  
 
-        if model is not None:  # Return the model if a valid one was found
+        if model is not None:  
             return (model, show_help,)
         else:
-            return (None, show_help,)  # Return None if no valid models were provided
+            return (None, show_help,)  
 
 
-NODE_CLASS_MAPPINGS = {  # <---Outdent these lines
+NODE_CLASS_MAPPINGS = {  
       "DynamicModelswitch": DynamicModelswitch,
 }
 
